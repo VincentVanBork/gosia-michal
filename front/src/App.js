f556a07a6660cffad8d0a0bbbb08cf4a760973e9
MDarowny <michal.darowny@gmail.com>
2021-10-02 15:11:59 +0200

    front work

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to out wedding invitation. We are the bebis Michał and Gosia.
         </p>
         <a
           className="App-link"
