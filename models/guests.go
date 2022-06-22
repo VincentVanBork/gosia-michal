22abd61b1fae28f9e52c21469809fffe9f73e167
MDarowny <michal.darowny@gmail.com>
2022-06-22 15:17:39 +0200

    table error defauilt

--- models/guests.go
+++ models/guests.go
@@ -23,5 +23,5 @@ type Invitation struct {
 	Hotel              bool `gorm:"default:false"`
 	Transport          bool `gorm:"default:false"`
 	Guests             []Guest
-	TableId            uint `gorm:"default:-1"`
+	TableId            int `gorm:"default:-1"`
 }
